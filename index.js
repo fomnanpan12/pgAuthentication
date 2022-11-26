a5b2d258b206d336856d3dd9ae4aa1640fac85e2
fomnanpan12 <fomnanpan12@gmail.com>
2022-11-26 12:10:49 +0100

    first commit

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const Pool = require('pg').Pool;
 const ejs = require('ejs');
+const Auth = require('./routes/authenticate');
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
 
+
+
+PORT = process.env.PORT || 3000;
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+})
 
 // app
 const app = express();
+
+// app.get('/authenticate', Auth.Signup);
+app.use(Auth);
+
+app.listen(PORT, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`app listening on port ${PORT}`);
+})
---
+++ routes/authenticate.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const Pool = require('pg').Pool;
+const ejs = require('ejs');
+
+// app
+var Router = express.Router();
+const app = express();
+app.set("view engine", 'ejs');
+app.set('views', './views');
+
+Router.get('/authenticate', (req, res) => {
+    res.render('index.ejs')
+})
+
+module.exports = Router;
